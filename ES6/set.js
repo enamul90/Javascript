e26be96329482ac24b95ef9beba9f16606a1adb4
enamul hossen <hfiroz1998@gmail.com>
2024-07-05 12:47:16 +0600

    hellow

---
+++ ES6/set.js
@@ -0,0 +1,14 @@
+// 01 basic set
+let mySet = new Set();
+mySet.add("color")
+mySet.add("size")
+mySet.add("price")
+mySet.add("quantity")
+
+console.log(mySet)
+
+
+// 02 set m2
+let serM2 = new Set(1,2,4,5,6,7,);
+console.log(serM2)
+
