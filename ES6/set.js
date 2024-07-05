c6c3f868d7f2adc29f70312fd150cec75ef08f50
md enamul <hfiroz1998@gmail.com>
2024-07-05 19:46:18 +0600

    new o

--- ES6/set.js
+++ ES6/set.js
@@ -8,7 +8,30 @@ mySet.add("quantity")
 console.log(mySet)
 
 
-// 02 set m2
-let serM2 = new Set(1,2,4,5,6,7,);
-console.log(serM2)
+// 02 set m2  (সেট তৈরির দ্বিতীয় নিয়ম)
+let setM2 = new Set([1,2,4,5,6,7,]);
+console.log(setM2)
 
+
+// 03 set clear
+setM2.clear()
+console.log(setM2)
+
+
+// 05 set delete
+mySet.delete("price")
+console.log(mySet)
+
+// 05 set size
+console.log(mySet.size)
+
+// 06 set values
+console.log(mySet.values())
+
+// 06 set has
+if(mySet.has("quantity")){
+    console.log("500TK")
+}
+else{
+    console.log("Error")
+}
