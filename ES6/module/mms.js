a7b35fc6c703779f040ac2d6eff9ebd0804b65dc
md enamul <hfiroz1998@gmail.com>
2024-07-06 21:08:47 +0600

    new

---
+++ ES6/module/mms.js
@@ -0,0 +1,13 @@
+const nameOf = "Enamul";
+
+function friendName(){
+    console.log("abul","babul","kabul")
+}
+
+class bangladesh{
+    dhaka(){
+        alert("me alert")
+    }
+}
+
+export {nameOf, friendName, bangladesh};
---
+++ ES6/module/nnp.js
@@ -0,0 +1,7 @@
+import { nameOf, friendName, bangladesh } from "./mms.js";
+
+
+console.log(nameOf)
+friendName()
+let ovj = new bangladesh();
+ovj.dhaka();
