4e9e36e3019bcde7d9dee545cc8e11d1619fddf9
enamul hossen <hfiroz1998@gmail.com>
2024-07-10 19:36:43 +0600

    new code

--- node/fastWeb/index.js
+++ node/fastWeb/index.js
@@ -1,18 +1,18 @@
 let fs = require("fs");
-let url = require("http");
+let http = require("http");
 
-let server = url.createServer(function(req, res){
-    if(req == "/"){
+let server = http.createServer(function(req, res){
+    if(req.url == "/"){
         let data = fs.readFileSync('home.html','utf8');
         res.end(data);
     }
 
-    else if(req == "/contact"){
+    else if(req.url == "/contact"){
         let data = fs.readFileSync('contact.html','utf8');
         res.end(data);
     }
 
-    else if(req == "/about"){
+    else if(req.url == "/about"){
         let data = fs.readFileSync('about.html','utf8');
         res.end(data);
     }
